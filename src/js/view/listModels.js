ff807b8cea010a7a8680e5ba295b9a60180e3f3f
Andy Ching <dogdoglization@gmail.com>
2019-06-30 13:51:13 +0800

    pick last model after list models

--- src/js/view/listModels.js
+++ src/js/view/listModels.js
@@ -5,10 +5,13 @@ import showModelSummary from "../model/showModelSummary";
 
 //list stored models in local storage | indexeddb
 export default async function listModels() {
+    //restore models from storage
     const models = await tf.io.listModels();
+    const modelUrls = Object.keys(models);
+    //render model options
     $("#model-list .model-option").remove();
-    for (let url in models) {
-        const $input = $(`<input type="radio" name="use-model" value=${url}>`).change(async function() {
+    for (let url of modelUrls) {
+        const $input = $(`<input type="radio" name="use-model" value="${url}">`).change(async function() {
             const model = await tf.loadLayersModel(url);
             setModel(model);
             showModelSummary(model);
@@ -19,10 +22,13 @@ export default async function listModels() {
                 await listModels(); //refresh
             }
         });
-        $("#model-list").append(
-            $(`<li class="model-option"></li>`)
-                .append($(`<label>${url}</label>`).prepend($input))
-                .append($removeBtn),
-        );
+        $(`<li class="model-option"></li>`)
+            .append($(`<label>${url}</label>`).prepend($input))
+            .append($removeBtn)
+            .appendTo("#model-list");
+    }
+    //select the last one
+    if (modelUrls.length > 0) {
+        $(`input[type="radio"][name="use-model"][value="${modelUrls[modelUrls.length - 1]}"]`).prop("checked", true).change();
     }
 }
